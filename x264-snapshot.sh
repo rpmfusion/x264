47f9efd601d7b5db44688c8978c1f7281dfa09f2
Sérgio M. Basto <sergio@serjux.com>
2021-10-01 00:01:40 +0100

    use 8 character for git hash (as gitlab do)

--- x264-snapshot.sh
+++ x264-snapshot.sh
@@ -20,7 +20,7 @@ pushd "$tmp"
 git clone https://code.videolan.org/videolan/x264.git -b ${branch}
 
 cd ${package}
-tag=$(git rev-list HEAD -n 1 | cut -c 1-7)
+tag=$(git rev-list HEAD -n 1 | cut -c 1-8)
 git checkout ${commit}
 git checkout -b rpmfusion
 ./version.sh > version.h
